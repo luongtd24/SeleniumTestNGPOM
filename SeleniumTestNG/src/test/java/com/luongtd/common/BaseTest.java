a65420885e52a91ce1e871c69c3fb2e3cfe98498
luongtd24 <ducluongp72402@gmail.com>
2023-11-23 14:25:18 +0700

    commit

--- SeleniumTestNG/src/test/java/com/luongtd/common/BaseTest.java
+++ SeleniumTestNG/src/test/java/com/luongtd/common/BaseTest.java
@@ -4,56 +4,54 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class BaseTest {
-    public WebDriver driver;
 
-    @BeforeMethod
-    public void createBrowser() {
+    public static WebDriver driver;
+
+    public static void createBrowser(){
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         driver = new ChromeDriver();
-        driver.manager().window().maximize();
-        driver.manager().timeout().implicitlyWait(Duration.ofSeconds(10));
-        driver.manager().timeout().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
     }
-        public void creatBrowser (String BrowserName){
-            System.setProperty("webdriver.http.factory", "jdk-http-client");
 
-            if (browserName.equals("chrome")) {
-                driver = new ChromeDriver();
-            }
-            if (browserName.equals("edge")) {
-                driver = new EdgeDriver();
-            }
-            if (browserName.equals("fireFox")) {
-                driver = new FirefoxDriver();
-            }
+    public static void createBrowser(String browserName){
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
 
-            driver.manager().window().maximize();
-            driver.manager().timeout().implicitlyWait(Duration.ofSeconds(10));
-            driver.manager().timeout().pageLoadTimeout(Duration.ofSeconds(10));
+        if(browserName.equals("chrome")){
+            driver = new ChromeDriver();
+        }
+        if(browserName.equals("edge")){
+            driver = new EdgeDriver();
+        }
+        if(browserName.equals("firefox")){
+            driver = new FirefoxDriver();
         }
 
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+    }
 
-        @AfterMethod
-        public void closeBrowser () {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            driver.quit();
+    public static void closeBrowser(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        public void sleep ( double second){
-            try {
-                Thread.sleep((long) (1000 * second));
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        driver.quit();
+    }
+
+    public static void sleep(double second){
+        try {
+            Thread.sleep((long) (1000 * second));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+    }
 
 }
